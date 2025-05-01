caecad07c0239fae5b8f1a7215e185fdda1cc0b2
Courage Zeng <czeng@lcs.on.ca>
2025-05-01 15:10:47 -0400

    Initial Commit

---
+++ Andantindex/AndantindexApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndantindexApp.swift
+//  Andantindex
+//
+//  Created by 曾梓恒 on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct AndantindexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
