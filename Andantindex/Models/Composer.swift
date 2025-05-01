a203e607d029aa0abfd2ea6e4068d0c622828d89
Courage Zeng <czeng@lcs.on.ca>
2025-05-01 15:28:28 -0400

    Created Composer model storing JSON data in Swift.

--- Andantindex/Models/Composer.swift
+++ Andantindex/Models/Composer.swift
@@ -7,3 +7,30 @@
 
 import Foundation
 
+// MARK: Composer Model
+
+struct ComposerList: Codable {
+    let composers: [Composer]
+}
+
+struct Composer: Codable, Identifiable {
+    let id: String
+    let name: String
+    let completeName: String
+    let birth: String
+    let death: String
+    let epoch: String
+    let portrait: String
+    
+    // Correctly map JSON keys to Swift property names
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case completeName = "complete_name"
+        case birth
+        case death
+        case epoch
+        case portrait
+    }
+}
